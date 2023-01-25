1e7035a72a4087f15a13a228a42fb9bc90e4a5df
chihunmanse <chihunmanse@gmail.com>
2023-01-25 14:37:19 +0900

    revert

---
+++ contracts/Revert.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Revert {
+    function revertWithoutMessage() external {
+        revert();
+    }
+
+    function revertWithMessage() external {
+        revert("revert message");
+    }
+
+    function requireWithoutMessage() external {
+        require(false);
+    }
+
+    function requireWithMessage() external {
+        require(false, "require message");
+    }
+
+    function revertByYul() external {
+        // 0x08c379a0
+        bytes4 errorSelector = bytes4(keccak256(bytes("Error(string)")));
+     
+        assembly {
+            mstore(0x00, errorSelector)
+            mstore(add(0x00, 4), 32) // data offset
+            mstore(add(0x00, 36), 14) // string length
+            mstore(add(0x00, 68), "revert message")
+            revert(0x00, 100)
+        }
+    }
+
+    error CustomError();
+    error CustomErrorWithParameter(uint256 param);
+
+    function revertCustomError() external {
+        revert CustomError();
+    }
+
+    function revertCustomErrorWithParameter() external {
+        revert CustomErrorWithParameter(1);
+    }
+
+    function revertCustomErrorByYul() external {
+        bytes4 errorSelector = CustomError.selector;
+
+        assembly {
+            mstore(0x00, errorSelector)
+            revert(0x00, 0x04)
+        }
+    }
+
+    function revertCustomErrorWithParameterByYul() external {
+        bytes4 errorSelector = CustomErrorWithParameter.selector;
+
+        assembly {
+            mstore(0x00, errorSelector)
+            mstore(add(0x00, 4), 1) // param uint256 value 1 padded to 32 bytes
+            revert(0x00, 36)
+        }
+    }
+}
---
+++ test/revert.ts
@@ -0,0 +1,144 @@
+/** @format */
+import { Contract } from "ethers";
+import { defaultAbiCoder } from "ethers/lib/utils";
+import { ethers, expect } from "hardhat";
+
+// method id of 'Error(string)'
+const ERROR_STRING_PREFIX = "0x08c379a0";
+
+const errorIface = new ethers.utils.Interface([
+  "error CustomError()",
+  "error CustomErrorWithParameter(uint256 param)",
+]);
+
+const decodeErrorData = (data: string) => {
+  if (data === "0x") {
+    return { kind: "Empty" };
+  } else if (data.startsWith(ERROR_STRING_PREFIX)) {
+    const encodedReason = data.slice(ERROR_STRING_PREFIX.length);
+
+    const reason = defaultAbiCoder.decode(["string"], `0x${encodedReason}`)[0];
+
+    return {
+      kind: "Error",
+      reason,
+    };
+  }
+
+  const sig = data.slice(0, 10);
+  const error = errorIface.getError(sig);
+
+  return {
+    kind: "Custom",
+    id: sig,
+    name: error.name,
+    data: errorIface.decodeErrorResult(error, data),
+  };
+};
+
+describe("Revert Test", () => {
+  let revert: Contract;
+
+  before(async () => {
+    const Revert = await ethers.getContractFactory("Revert");
+    revert = await Revert.deploy();
+    await revert.deployed();
+  });
+
+  describe("Error", () => {
+    it("Revert Without Message", async () => {
+      try {
+        await revert.revertWithoutMessage();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert without message:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Revert With Message", async () => {
+      try {
+        await revert.revertWithMessage();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert with message:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Require Without Message", async () => {
+      try {
+        await revert.requireWithoutMessage();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("require without message:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Require With Message", async () => {
+      try {
+        await revert.requireWithMessage();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("require with message:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Revert By Yul", async () => {
+      try {
+        await revert.revertByYul();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert by yul:", decodeErrorData(error.data));
+      }
+    });
+  });
+
+  describe("Custom Error", () => {
+    it("Revert Custom Error", async () => {
+      try {
+        await revert.revertCustomError();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert custom error:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Revert Custom Error With Parameter", async () => {
+      try {
+        await revert.revertCustomErrorWithParameter();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log(
+          "revert custom error with parameter:",
+          decodeErrorData(error.data)
+        );
+      }
+    });
+
+    it("Revert Custom Error By Yul", async () => {
+      try {
+        await revert.revertCustomErrorByYul();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert custom error by yul:", decodeErrorData(error.data));
+      }
+    });
+
+    it("Revert Custom Error With Parameter By Yul", async () => {
+      try {
+        await revert.revertCustomErrorWithParameterByYul();
+
+        console.log("success");
+      } catch (error: any) {
+        console.log("revert custom error by yul:", decodeErrorData(error.data));
+      }
+    });
+  });
+});
