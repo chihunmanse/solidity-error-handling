147614a34143c9de265b4f728ac7207b730afa2e
chihunmanse <chihunmanse@gmail.com>
2023-06-01 18:10:54 +0900

    add: forge assert test

--- test/Assert.t.sol
+++ test/Assert.t.sol
@@ -15,4 +15,40 @@ contract AssertTest is Test {
         vm.expectRevert(stdError.assertionError);
         asserter.assert0x1();
     }
+
+    function testArithmeticError() external {
+        vm.expectRevert(stdError.arithmeticError);
+        asserter.assert0x11();
+    }
+
+    function testDivisionError() external {
+        vm.expectRevert(stdError.divisionError);
+        asserter.assert0x12();
+    }
+
+    function testEnumConversionError() external {
+        vm.expectRevert(stdError.enumConversionError);
+        asserter.assert0x21();
+    }
+
+    function testPopError() external {
+        vm.expectRevert(stdError.popError);
+        asserter.assert0x31();
+    }
+
+    function testIndexOOBError() external {
+        vm.expectRevert(stdError.indexOOBError);
+        asserter.assert0x32();
+    }
+
+    function testRevertOpcode() external {
+        vm.expectRevert(stdError.assertionError);
+        asserter.assertRevertOpcode();
+    }
+
+    function testInvalidOpcode() external {
+        bytes memory revertData;
+        vm.expectRevert(revertData);
+        asserter.assertInvalidOpcode();
+    }
 }
